491fb60a82e4051fe7cb6f4f9002fbae6757894a
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-19 09:36:43 -0300

    194 - Componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Main from "./components/Main";
 import "./App.css";
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return <Main />;
 }
 
 export default App;

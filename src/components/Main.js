0252209f1683f6b6b1a30bf62380b4db3e220a3c
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-19 10:32:06 -0300

    194 - Componentes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import "./Main.css";
 
 export default class Main extends Component {
+  state = {
+    novaTarefa: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
   render() {
-    return <h1>Meu componente com estado</h1>;
+    const { novaTarefa } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <form action="#">
+          <input onChange={this.handleChange} type="text" />
+          <button type="submit">Enviar</button>
+        </form>
+
+        <h3>{novaTarefa}</h3>
+      </div>
+    );
   }
 }
